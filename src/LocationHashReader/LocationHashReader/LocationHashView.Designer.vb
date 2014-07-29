2dbb2f6fe85dfbec1bbf3bb149d837be1248efc5
RobRobertsCE <rroberts@centeredgesoftware.com>
2014-07-29 15:35:15 -0400

    Location Hash Viewer

---
+++ src/LocationHashReader/LocationHashReader/LocationHashView.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LocationHashView
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.getHashButton = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.locationComboBox = New System.Windows.Forms.ComboBox()
+        Me.copyHashButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'getHashButton
+        '
+        Me.getHashButton.Enabled = False
+        Me.getHashButton.Location = New System.Drawing.Point(447, 40)
+        Me.getHashButton.Name = "getHashButton"
+        Me.getHashButton.Size = New System.Drawing.Size(75, 23)
+        Me.getHashButton.TabIndex = 0
+        Me.getHashButton.Text = "Get Hash"
+        Me.getHashButton.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(14, 69)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(508, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'locationComboBox
+        '
+        Me.locationComboBox.FormattingEnabled = True
+        Me.locationComboBox.Location = New System.Drawing.Point(14, 42)
+        Me.locationComboBox.Name = "locationComboBox"
+        Me.locationComboBox.Size = New System.Drawing.Size(427, 21)
+        Me.locationComboBox.TabIndex = 2
+        '
+        'copyHashButton
+        '
+        Me.copyHashButton.Enabled = False
+        Me.copyHashButton.Location = New System.Drawing.Point(447, 95)
+        Me.copyHashButton.Name = "copyHashButton"
+        Me.copyHashButton.Size = New System.Drawing.Size(75, 23)
+        Me.copyHashButton.TabIndex = 3
+        Me.copyHashButton.Text = "Copy Hash"
+        Me.copyHashButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(90, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Select a Location"
+        '
+        'LocationHashView
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(534, 130)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.copyHashButton)
+        Me.Controls.Add(Me.locationComboBox)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.getHashButton)
+        Me.Name = "LocationHashView"
+        Me.Text = "SiteInfo Location Hash Reader"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents getHashButton As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents locationComboBox As System.Windows.Forms.ComboBox
+    Friend WithEvents copyHashButton As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ src/LocationHashReader/LocationHashReader/LocationHashView.vb
@@ -0,0 +1,129 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports PFSConnectLibrary
+Imports System.IO
+Imports System.Text
+Imports System.Collections
+Imports System.Collections.Generic
+Imports System.ComponentModel
+
+Public Class LocationHashView
+
+#Region " locals "
+    Protected Structure LocationHashStruct
+        Public Property LocationName As String
+        Public Property Address1 As String
+        Public Property City As String
+        Public Property State As String
+        Public Property ZipCode As String
+    End Structure
+
+    Private _locations As BindingList(Of LocationHashStruct) = Nothing
+
+#End Region
+
+#Region " load / init "
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Try
+            LoadLocations()
+
+            locationComboBox.DisplayMember = "LocationName"
+            locationComboBox.DataSource = _locations
+
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString())
+
+        End Try
+    End Sub
+
+    Private Sub LoadLocations()
+        Dim builder As New StringBuilder()
+
+        _locations = New BindingList(Of LocationHashStruct)
+
+        Dim sql As String = "SELECT LocationName, Address1, City, State, ZipCode FROM LocationBase"
+
+        Dim dbModel As New DatabaseModel("ceserver", "siteinfo", "sa", "sql")
+
+        Dim cnStr = ConnectionStringFactory.GetSqlConnectionString(dbModel)
+
+        Using cn As New SqlConnection(cnStr)
+            cn.Open()
+
+            Using cmd As New SqlCommand(sql, cn)
+                Using dr = cmd.ExecuteReader()
+                    While dr.Read
+                        Dim location As New LocationHashStruct()
+
+                        location.LocationName = dr(0).ToString()
+                        location.Address1 = dr(1).ToString()
+                        location.City = dr(2).ToString()
+                        location.State = dr(3).ToString()
+                        location.ZipCode = dr(4).ToString()
+
+                        _locations.Add(location)
+
+                    End While
+                End Using
+            End Using
+        End Using
+    End Sub
+
+#End Region
+
+#Region " control event handlers "
+    Private Sub locationComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles locationComboBox.SelectedIndexChanged
+        Me.getHashButton.Enabled = Not (locationComboBox.SelectedItem Is Nothing)
+    End Sub
+
+    Private Sub getHashButton_Click(sender As Object, e As EventArgs) Handles getHashButton.Click
+
+        If (locationComboBox.SelectedItem Is Nothing) Then Return
+
+        TextBox1.Text = GetLocationHash(locationComboBox.SelectedItem)
+
+        copyHashButton.Enabled = Not (String.IsNullOrEmpty(TextBox1.Text))
+
+    End Sub
+
+    Private Sub copyHashButton_Click(sender As Object, e As EventArgs) Handles copyHashButton.Click
+        TextBox1.SelectAll()
+        TextBox1.Copy()
+    End Sub
+
+#End Region
+
+#Region " location hash methods "
+
+    Private Function GetLocationHash(location As LocationHashStruct) As String
+
+        Dim builder As New StringBuilder()
+
+        builder.Append(location.LocationName.Trim())
+        builder.Append("~"c)
+        builder.Append(location.Address1.Trim())
+        builder.Append("~"c)
+        builder.Append(location.City.Trim())
+        builder.Append("~"c)
+        builder.Append(location.State.Trim())
+        builder.Append("~"c)
+        builder.Append(location.ZipCode.Trim())
+
+        Dim bytes() As Byte = Encoding.UTF8.GetBytes(builder.ToString())
+
+        Dim hash As New System.Security.Cryptography.MD5CryptoServiceProvider()
+        bytes = hash.ComputeHash(bytes)
+
+        builder = New StringBuilder()
+
+        For Each b As Byte In bytes
+            builder.Append(b.ToString("x").PadLeft(2, "0"c))
+        Next
+
+        Return builder.ToString()
+
+    End Function
+
+#End Region
+
+End Class
